4d4f4b4633bb17a3212dacb8410e334477f47f87
Jani Nykänen <jnyknn@gmail.com>
2018-08-30 18:20:08 +0300

    Button class

---
+++ src/application/ui/Button.java
@@ -0,0 +1,120 @@
+package application.ui;
+
+import core.utility.VoidCallback;
+
+/**
+ * A button
+ * @author Jani Nykänen
+ *
+ */
+public class Button {
+
+	/** Text */
+	private String text;
+	
+	/** Is disabled */
+	private boolean disabled = false;
+	
+	/** Called when "pressed" */
+	private VoidCallback cb = null;
+	
+	
+	/**
+	 * Constructor
+	 * @param text Text
+	 * @param cb Callback
+	 */
+	public Button(String text, VoidCallback cb) {
+		
+		this.text = text;
+		this.cb = cb;
+		disabled = false;
+	}
+	
+	
+	/**
+	 * Constructor
+	 * @param text Text
+	 */
+	public Button(String text) {
+		
+		this(text, null);
+	}
+	
+	
+	/**
+	 * Constructor
+	 */
+	public Button() {
+		
+		this("", null);
+	}
+	
+	
+	/**
+	 * Disable button
+	 */
+	public void disable() {
+		
+		disabled = true;
+	}
+	
+	
+	/**
+	 * Enable
+	 */
+	public void enable() {
+		
+		disabled = false;
+	}
+	
+	
+	/**
+	 * Is the button disabled
+	 * @return True, if disabled
+	 */
+	public boolean isDisabled() {
+		
+		return disabled;
+	}
+	
+	
+	/**
+	 * Set a new text
+	 * @param text New text
+	 */
+	public void setText(String text) {
+		
+		this.text = text;
+	}
+	
+	
+	/**
+	 * Get text
+	 * @return Text
+	 */
+	public String getText() {
+		
+		return text;
+	}
+	
+	
+	/**
+	 * Set a new callback function
+	 * @param cb New callback function
+	 */
+	public void setCallback(VoidCallback cb) {
+		
+		this.cb = cb;
+	}
+	
+	
+	/**
+	 * Call the callback function
+	 */
+	public void activate() {
+		
+		if(cb != null)
+			cb.execute();
+	}
+}
---
+++ src/application/ui/VerticalButtonList.java
@@ -0,0 +1,18 @@
+package application.ui;
+
+import java.util.List;
+
+/**
+ * A vertical list of buttons
+ * @author Jani Nykänen
+ *
+ */
+public class VerticalButtonList {
+
+	/** Amount of buttons */
+	private int length;
+	
+	/** A list of buttons */
+	private List<Button> buttons;
+	
+}
